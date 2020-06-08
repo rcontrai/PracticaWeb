bc3a7bc58bafb6d530ca7b16c25a25b72744fd30
Romain <romain.contrain@etu.enseeiht.fr>
2020-06-09 00:47:03 +0200

    trying to fix some relative urls

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js')
 
-console.log(`Workbox ${workbox ? 'sí' : 'no'} está funcionando`)
+//console.log(`Workbox ${workbox ? 'sí' : 'no'} está funcionando`)
 
 workbox.precaching.precacheAndRoute([
-  {url: '/index.html', revision: null },
-  {url: '/app.js', revision: null},
+  {url: 'PracticaWeb/index.html', revision: null },
+  {url: 'PracticaWeb/app.js', revision: null},
 ])
